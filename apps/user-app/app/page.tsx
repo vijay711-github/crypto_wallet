7f11ba89caa748337dcd35c190780ec68e7a7643
vijay711-github <vj7112000@gmail.com>
2024-08-09 23:38:06 +0530

    Updated the Tailwind and updated the ui package:modification

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Button } from "@repo/ui/button";
+
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center animate-bounce">
+
+      <div className="text-xl rounded-md bg-slate-200 w-32 text-center animate-pulse text-violet-500">TurboRepo</div>
+      </div>
+  );
+}
